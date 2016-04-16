c902445fc9bc29011f06d03bf311dd48ff7d0fc2
Nathan Plowman <nplowman@gmail.com>
2016-04-15 22:37:15 -0600

    Completed gridview template.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,14 +10,38 @@ angular.module('sudokuSolver.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', ['$scope', function($scope) {
-  $scope.rows = 9;
-  $scope.cols = 9;
+  $scope.cells = [];
+  $scope.rows = [];
+  $scope.cols = [];
+
+  $scope.getCells = function() {
+    $scope.cells = [];
+    for (var x = 0; x < 9; x++) {
+      for (var y = 0; y < 9; y++) {
+        $scope.cells.push({
+          col: x,
+          row: y,
+          val: ''
+        });
+      }
+    }
+  };
 
   $scope.getRows = function() {
-    return new Array($scope.rows);
+    $scope.rows = [];
+    for (var i = 0; i < $scope.cells.length; i++) {
+      var cell = $scope.cells[i];
+      var cell_row = cell.row;
+      if ($scope.rows[cell_row] === undefined) {
+        $scope.rows[cell_row] = {cells: []};
+      }
+      $scope.rows[cell_row].cells.push(cell);
+    }
+  };
+
+  $scope.init = function() {
+    $scope.getCells();
+    $scope.getRows();
   }
 
-  $scope.getCols = function() {
-    return new Array($scope.cols);
-  }
 }]);
