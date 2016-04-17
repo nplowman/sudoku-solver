2ec0dd41d2e330eb79a90210c67cdf41dbb9ee0b
Nathan Plowman <nplowman@gmail.com>
2016-04-17 10:05:10 -0600

    Added row and col checking to algorithm.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -21,11 +21,28 @@ angular.module('sudokuSolver.view1', ['ngRoute'])
         $scope.cells.push({
           col: x,
           row: y,
+          box: $scope.getBoxNumber(x, y),
           val: '',
           highlight: false
         });
       }
     }
+  };
+
+  $scope.getBoxNumber = function(x, y) {
+    for (var box_number = 0; box_number < 9; box_number++) {
+      var limits = {
+        start_x: (box_number % 3) + (box_number % 3 * 2),
+        end_x: (box_number % 3) + (box_number % 3 * 2) + 2,
+        start_y: Math.floor(box_number/3) + Math.floor(box_number/3) * 2,
+        end_y: Math.floor(box_number/3) + Math.floor(box_number/3) * 2 + 2
+      };
+
+      if (x >= limits.start_x && x<= limits.end_x && y >= limits.start_y && y <= limits.end_y) {
+        break;
+      }
+    }
+    return box_number;
   };
 
   $scope.getRows = function() {
@@ -41,22 +58,24 @@ angular.module('sudokuSolver.view1', ['ngRoute'])
   };
 
   $scope.solvePuzzle = function() {
-    //Cross-elimination test
-    for (var target_number = 1; target_number < 10; target_number++) {
-      $scope.checkBoxes(target_number);
-
+    do {
+      for (var target_number = 1; target_number < 10; target_number++) {
+        $scope.checkBoxes(target_number);
+        $scope.checkRows(target_number);
+        $scope.checkCols(target_number);
+      }
     }
+    while($scope.puzzleIsUnsolved());
   };
 
   $scope.checkBoxes = function(target_number) {
-    var black_list = $scope.getBlacklist(target_number);
     for (var box_number = 0; box_number < 9; box_number ++) {
       var box = $scope.getBox(box_number);
       var empty_candidate_cells = [];
 
       for (var i = 0; i < box.cells.length; i++) {
         var cell = box.cells[i];
-        if (cell.val == '' && black_list.rows.indexOf(cell.row) == -1 && black_list.cols.indexOf(cell.col) == -1) {
+        if (cell.val == '' && !$scope.cellIsBlacklisted(cell, target_number)) {
           empty_candidate_cells.push(cell);
         }
       }
@@ -65,6 +84,42 @@ angular.module('sudokuSolver.view1', ['ngRoute'])
         empty_candidate_cells[0].val = target_number;
         empty_candidate_cells[0].highlight = true;
       }
+    }
+  };
+
+  $scope.checkRows = function(target_number) {
+    var empty_candidate_cells = [];
+    for (var row_number = 0; row_number < 9; row_number ++) {
+      var row = $scope.getRow(row_number);
+      for (var i = 0; i < row.cells.length; i++) {
+        var cell = row.cells[i];
+        if (cell.val == '' && !$scope.cellIsBlacklisted(cell, target_number)) {
+          empty_candidate_cells.push(cell);
+        }
+      }
+    }
+
+    if (empty_candidate_cells.length == 1) {
+      empty_candidate_cells[0].val = target_number;
+      empty_candidate_cells[0].highlight = true;
+    }
+  };
+
+  $scope.checkCols = function(target_number) {
+    var empty_candidate_cells = [];
+    for (var col_number = 0; col_number < 9; col_number ++) {
+      var col = $scope.getCol(col_number);
+      for (var i = 0; i < col.cells.length; i++) {
+        var cell = col.cells[i];
+        if (cell.val == '' && !$scope.cellIsBlacklisted(cell, target_number)) {
+          empty_candidate_cells.push(cell);
+        }
+      }
+    }
+
+    if (empty_candidate_cells.length == 1) {
+      empty_candidate_cells[0].val = target_number;
+      empty_candidate_cells[0].highlight = true;
     }
   };
 
@@ -86,6 +141,36 @@ angular.module('sudokuSolver.view1', ['ngRoute'])
     return box;
   };
 
+  //Check to see if cell is on 'blacklist' for a given target number.
+  //Being on the "blacklist" means this cell cannot possibly contain the target number,
+  //since another cell in the same column or row does already.
+  $scope.cellIsBlacklisted = function(cell, target_number) {
+    var black_list = $scope.getBlacklist(target_number);
+    if (black_list.rows.indexOf(cell.row) != -1
+      || black_list.cols.indexOf(cell.col) != -1
+      || black_list.boxes.indexOf(cell.box) != -1) {
+      return true;
+    }
+  };
+
+  $scope.getRow = function(row_number) {
+    var row = {id: row_number, cells: []};
+    for (var col = 0; col < 9; col++) {
+      var cell = $scope.getCell(col, row_number);
+      row.cells.push(cell);
+    }
+    return row;
+  };
+
+  $scope.getCol = function(col_number) {
+    var col = {id: col_number, cells: []};
+    for (var row = 0; row < 9; row++) {
+      var cell = $scope.getCell(col_number, row);
+      col.cells.push(cell);
+    }
+    return col;
+  };
+
   $scope.getCell = function(x, y) {
     for (var i = 0; i < $scope.cells.length; i++) {
       var cell = $scope.cells[i];
@@ -97,7 +182,9 @@ angular.module('sudokuSolver.view1', ['ngRoute'])
 
   //Returns list of rows and columns which already have instance of the target number.
   $scope.getBlacklist = function(target_number) {
-    var blacklist = {rows: [], cols: []};
+    var blacklist = {rows: [], cols: [], boxes: []};
+
+    //Add rows and columns to blacklist
     for (var i = 0; i < $scope.cells.length; i++) {
       var cell = $scope.cells[i];
       if (cell.val == target_number) {
@@ -109,7 +196,28 @@ angular.module('sudokuSolver.view1', ['ngRoute'])
         }
       }
     }
+
+    //Add boxes to blacklist
+    for (var box_number = 0; box_number < 9; box_number++) {
+      var box = $scope.getBox(box_number);
+      for (i = 0; i < box.cells.length; i++) {
+        cell = box.cells[i];
+        if (cell.val == target_number && blacklist.boxes.indexOf(box_number) == -1) {
+          blacklist.boxes.push(box_number);
+        }
+      }
+    }
+
     return blacklist;
+  };
+
+  $scope.puzzleIsUnsolved = function(){
+    for (var i = 0; i < $scope.cells.length; i++) {
+      var cell = $scope.cells[i];
+      if (cell.val == '') {
+        return true;
+      }
+    }
   };
 
   $scope.init = function() {
